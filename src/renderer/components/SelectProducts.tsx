24144d85301c2775c41186bd80dd7b0103d2bb13
Ashwin-DevAsh <2017ashwin@gmail.com>
2022-04-04 00:11:50 +0530

    added searchbar

--- src/renderer/components/SelectProducts.tsx
+++ src/renderer/components/SelectProducts.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { alpha, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -14,7 +14,7 @@ import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
 import {includes,remove, pull} from 'lodash'
 import ProductService from 'renderer/Services/ProductService';
-import { Switch } from '@material-ui/core';
+import { InputBase, Switch } from '@material-ui/core';
 
 
 
@@ -27,6 +27,43 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     flex: 1,
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
 }));
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -36,6 +73,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 export default function SelectProduct({open,onClose}) {
   const productService = new ProductService()
   const classes = useStyles();
+  const [searchValue,setSearchValue] = useState('')
 
   const handleClose = () => {
     onClose(false);
@@ -74,20 +112,40 @@ export default function SelectProduct({open,onClose}) {
       <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
         <AppBar className={classes.appBar}>
           <Toolbar>
+
             <Typography variant="h6" className={classes.title}>
               Select Products
             </Typography>
+            <div className={classes.search}>
+            <InputBase
+              placeholder="Search…"
+              value={searchValue}
+              onChange={(e)=>{setSearchValue(e.target.value)}}
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
             <Button autoFocus color="inherit" onClick={handleClose}>
               Close
             </Button>
+
           </Toolbar>
+
         </AppBar>
         <List>
           {
              allProducts.map((item,index)=>{
+
+               if(!item.productname.replaceAll(" ","").toLocaleLowerCase().includes(searchValue.toLocaleLowerCase().replaceAll(" ",""))){
+                 return <div></div>
+               }
                return(<div>
                   <ListItem key={index} button >
-                   <ListItemText primary={item.productname} secondary={item.category}/>
+                   <ListItemText primary={`${item.productname} - Rs.${item.price}`} secondary={item.category}/>
                    <Switch
                     defaultChecked={ includes(getAvailableProducts(),  item.productid )}
                     onChange={(e)=>{handleToggle(item.productid,e.target.checked)}}
